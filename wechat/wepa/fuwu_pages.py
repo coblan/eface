c6d5e6bb55cc19a5ea2042c67ed3aca314df1136
he_yulin <he_yulin@163.com>
2019-01-03 17:01:16 +0800

    debug

--- wechat/wepa/fuwu_pages.py
+++ wechat/wepa/fuwu_pages.py
@@ -92,6 +92,7 @@ class FuWuHao(object):
         url='https://api.weixin.qq.com/sns/userinfo?access_token=%(access_token)s&openid=%(openid)s&lang=zh_CN'\
             %{'access_token':token,'openid':openid}
         resp=requests.get(url)
+        resp.encoding ='utf-8'
         dc= json.loads(resp.text)
         if dc.get('errcode'):
             raise UserWarning('获取用户信息错误:%s'%resp.text)
