9639735141cd00aed01de9a8374187442d1e7343
he_yulin <he_yulin@163.com>
2019-01-23 00:06:27 +0800

    debug

--- wechat/jssdk.py
+++ wechat/jssdk.py
@@ -1,3 +1,19 @@
+from django.conf import settings
+import requests
+import json
 
-def get_signature(url):
-    pass
+def get_config_parameter(url):
+    access_token = _get_signature(url)
+    
+    
+def _get_signature(url):
+    dc ={
+        'APPID':settings.WX_APPID,
+        'APPSECRET':settings.WX_APPSECRET
+    }
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%(APPID)s&secret=%(APPSECRET)s'%dc
+    rt = requests.get(url)
+    dc = json.loads(rt.text)
+    print(dc.get(k))
+    return dc.get('access_token')
+    
