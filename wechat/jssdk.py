95f873e9f582bbf1d3c58dd31d8c766867c5279a
he_yulin <he_yulin@163.com>
2019-01-23 16:34:07 +0800

    改进

--- wechat/jssdk.py
+++ wechat/jssdk.py
@@ -1,12 +1,33 @@
 from django.conf import settings
 import requests
 import json
+from helpers.director.decorators.cache_redis import cache_redis
+import time
+from helpers.func.random_str import get_str
+import hashlib
 
 def get_config_parameter(url):
-    access_token = _get_signature(url)
+    access_token = _get_access_token()
+    ticket = _get_ticket(access_token)
+    params = {
+        'noncestr':get_str(),
+        'jsapi_ticket':ticket,
+        'timestamp':int(time.time()),
+        'url':url,
+        
+    }
+    sign = _params_sign(params)
+    return {
+        'appId':settings.WX_APPID,
+        'signature':sign,
+        'noncestr':params['noncestr'],
+        'jsapi_ticket':params['jsapi_ticket'],
+        'timestamp':params['timestamp'],        
+    }
     
-    
-def _get_signature(url):
+
+@cache_redis(ex=7000)
+def _get_access_token():
     dc ={
         'APPID':settings.WX_APPID,
         'APPSECRET':settings.WX_APPSECRET
@@ -14,6 +35,25 @@ def _get_signature(url):
     url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%(APPID)s&secret=%(APPSECRET)s'%dc
     rt = requests.get(url)
     dc = json.loads(rt.text)
-    print(dc.get(k))
-    return dc.get('access_token')
-    
+    return dc['access_token']
+
+@cache_redis(ex=7000)
+def _get_ticket(access_token):
+    dc={
+        'access_token':access_token
+    }
+    url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=%(access_token)s&type=jsapi'%dc
+    rt = requests.get(url)
+    rt_dc = json.loads(rt.text)
+    return rt_dc['ticket']
+
+
+def _params_sign(params):
+    sign_str = ''
+    for k,v in sorted(params.items(),key=lambda p:p[0]):
+        if v:
+            sign_str += '{key}={value}&'.format(key=k,value=v)
+    sign_str = sign_str.rstrip('&')
+    #sign_str = sign_str + 'key=' + APISECERT
+    #return hashlib.md5(sign_str.encode('utf-8')).hexdigest().upper()   
+    return hashlib.sha1(sign_str.encode('utf-8')).hexdigest()
