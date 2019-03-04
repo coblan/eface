85554da4bd3bfb7ae550572d448d0576ecc406a7
he_yulin <he_yulin@163.com>
2019-03-04 13:58:03 +0800

    改进

--- wechat/wepa/fuwu_pages.py
+++ wechat/wepa/fuwu_pages.py
@@ -168,14 +168,6 @@ def is_subscribe(openid):
     dc = json.loads(rt.text)
     return dc['subscribe'] ==1
     
-#@cache_redis(ex=7000)
-#def get_access_token():
-    #args = {'appid':settings.WX_APPID,'secret':settings.WX_APPSECRET}
-    #url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%(appid)s&secret=%(secret)s"
-    #url = url % args
-    #rt = requests.get(url)
-    #dc = json.loads(rt.text)
-    #return dc['access_token']
 
 wechat_page_dc.update({
     'login':FuWuHaoLogin,
