58f6bc5ba1b69740fb3a52ac6006f327d7dd8e46
he_yulin <he_yulin@163.com>
2019-06-15 12:31:55 +0800

    改进

--- wechat/engin.py
+++ wechat/engin.py
@@ -1,8 +1,10 @@
 from helpers.director.engine import BaseEngine
 from .base_data import wechat_page_dc
+from helpers.director.base_data import site_cfg
 
 class WechatEngine(BaseEngine):
     need_login=False
     access_from_internet=True
 
 WechatEngine.add_pages(wechat_page_dc)
+site_cfg['inspect_dict']['wechat_page_dc'] = wechat_page_dc
