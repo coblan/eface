29096831387a18cff60052af826a1c3de70f6da5
hyl <coblan@163.com>
2021-06-29 23:13:44 +0800

    改进微信订单

--- wechat/admin_wxorder.py
+++ wechat/admin_wxorder.py
@@ -1,5 +1,5 @@
 from .models import TWXOrder
-from helpers.director.shortcut import TablePage,ModelTable,page_dc,director,RowFilter
+from helpers.director.shortcut import TablePage,ModelTable,page_dc,director,RowFilter,SelectSearch
 
 class WXorderPage(TablePage):
     def get_label(self):
@@ -12,9 +12,29 @@ class WXorderPage(TablePage):
         model = TWXOrder
         exclude =[]
         
+        def dict_head(self, head):
+            width = {
+                'user':150,
+                'transaction_id':100,
+            }
+            if head['name'] in width:
+                head['width'] = width[head['name']]
+            if head['name']=='id':
+                head['after_fields'] = ['user','confirmed','total_fee']
+            return head
+        
         class filters(RowFilter):
             names =['confirmed']
             range_fields =['create_time']
+        
+        class search(SelectSearch):
+            names = ['user__first_name']
+            
+            def get_option(self, name):
+                dc = {
+                    'user__first_name':'用户'
+                }
+                return {'value':name,'label':dc[name]}
 
 director.update({
     'wxorder':WXorderPage.tableCls,
