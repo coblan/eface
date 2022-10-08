c8318925af7e1b6712db63a75c36ea0caa08d9b8
coblan <coblan@163.com>
2022-10-09 00:05:56 +0800

    dd

--- mylbs/admin.py
+++ mylbs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from . import js_cfg
-
-from .field_proc import point_proc
+from django.conf import settings
+from helpers.func.dot_dict import read_dict_path
+if read_dict_path( settings,'DATABASES.ENGINE') =='django.contrib.gis.db.backends.postgis':
+    from .field_proc import point_proc
