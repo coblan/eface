a03cbfa901e0364a5240da0300dc957eca7f6dff
coblanplus <coblanplus@gmail.com>
2023-07-24 15:49:00 +0800

    fhh

--- mylbs/admin.py
+++ mylbs/admin.py
@@ -6,3 +6,4 @@ from django.conf import settings
 from helpers.func.dot_dict import read_dict_path
 if read_dict_path( settings,'DATABASES.default.ENGINE') in ['django.contrib.gis.db.backends.postgis','django.contrib.gis.db.backends.mysql']:
     from .field_proc import point_proc
+    from . field_proc import polygen_proc
